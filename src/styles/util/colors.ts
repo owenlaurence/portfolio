a17e554238c075463ee5ce531444ab28ff6d4df9
stu <eoghan.laurence.12@gmail.com>
2026-04-22 21:15:36 -0500

    tag borders.

--- src/styles/util/colors.ts
+++ src/styles/util/colors.ts
@@ -1,20 +1,20 @@
 export const tagColors: Record<string, string> = {
-  devops: "bg-blue-100 text-blue-700",
-  github: "bg-blue-100 text-blue-700",
-  oauth: "bg-red-100 text-red-700",
-  oidc: "bg-red-100 text-red-700",
-  web: "bg-purple-100 text-purple-700",
-  android: "bg-purple-100 text-purple-700",
-  ios: "bg-purple-100 text-purple-700",
-  ci: "bg-green-100 text-green-700",
-  ssr: "bg-yellow-100 text-yellow-700",
-  react: "bg-yellow-100 text-yellow-700",
+  devops: "bg-blue-100 text-blue-700 border-1 border-blue",
+  github: "bg-blue-100 text-blue-700 border-1 border-blue",
+  oauth: "bg-red-100 text-red-700 border-1 border-red",
+  oidc: "bg-red-100 text-red-700 border-1 border-red",
+  web: "bg-purple-100 text-purple-700 border-1 border-purple",
+  android: "bg-purple-100 text-purple-700 border-1 border-purple",
+  ios: "bg-purple-100 text-purple-700 border-1 border-purple",
+  ci: "bg-green-100 text-green-700 border-1 border-green",
+  ssr: "bg-yellow-100 text-yellow-700 border-1 border-yellow",
+  react: "bg-yellow-100 text-yellow-700 border-1 border-yellow",
 }
 
 export const getTagColor = (tag: string) => {
   
   const cleanTag = tag.split("/")[0].toLowerCase();
 
-  return tagColors[cleanTag] ?? "bg-gray-100 text-gray-700 border-1 border-back";
+  return tagColors[cleanTag] ?? "bg-gray-100 text-gray-700 border-1 border-gray-700";
 }
   
